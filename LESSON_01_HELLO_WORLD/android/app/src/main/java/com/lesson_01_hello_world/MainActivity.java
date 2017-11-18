a9af0f30be46b4ba1bd1434ee8d6be44831263ed
erictherobot <erictherobot@gmail.com>
2017-11-17 23:03:24 -0500

    Adding hello world app

---
+++ LESSON_01_HELLO_WORLD/android/app/src/main/java/com/lesson_01_hello_world/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lesson_01_hello_world;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "LESSON_01_HELLO_WORLD";
+    }
+}
---
+++ LESSON_01_HELLO_WORLD/index.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+
+AppRegistry.registerComponent('LESSON_01_HELLO_WORLD', () => App);
